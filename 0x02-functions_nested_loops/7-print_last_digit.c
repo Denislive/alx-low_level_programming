289bd0f1d37b0b3cd4699d388751d49cf912d704
Denislive <dennygachemi6@gmail.com>
2023-06-20 14:52:08 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,25 +10,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-
-	if (n == INT_MIN)
-	{
-		last_digit = 8;
-		_putchar('8');
-		return (last_digit);
-	}
-
-	if (n < 0)
-		n = -n;
-
 	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
 	_putchar(last_digit + '0');
 
-	return (last_digit);
+	return last_digit;
 }

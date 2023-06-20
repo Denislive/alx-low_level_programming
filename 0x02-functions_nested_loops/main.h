18d82e95e118a2d22c1daccd19dc294345c2c131
Denislive <dennygachemi6@gmail.com>
2023-06-20 14:38:13 -0700

    last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
-int _abs(int n);
-int _isalpha(int c);
-int _islower(int c);
 int _putchar(char c);
+int _abs(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
+int print_last_digit(int n);
 
 #endif /* MAIN_H */

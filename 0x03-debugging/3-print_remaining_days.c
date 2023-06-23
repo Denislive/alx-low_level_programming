34434fdb0354ed5704b5667cbeeda6f2833b58c4
Denislive <dennygachemi6@gmail.com>
2023-06-22 17:00:05 -0700

    leap

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int leap = 0;
 
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
@@ -31,6 +31,11 @@ void print_remaining_days(int month, int day, int year)
     for (int i = 1; i < month; i++)
     {
         day_of_year += days_in_month[i];
+    }
+
+    if (leap && month > 2)
+    {
+        day_of_year++;
     }
 
     int days_remaining = 365 - day_of_year;

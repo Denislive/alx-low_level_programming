2af9d635d3d5b83a124347e0fb369e3111998e05
Denislive <dennygachemi6@gmail.com>
2023-06-26 14:23:42 -0700

    puts every char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,9 +4,8 @@
  * puts2 - Prints every other character of a string, starting with the first character.
  * @str: Pointer to the string to be printed.
  *
- * Description: This function takes a string pointer as a parameter and
- * prints every other character of the string, starting with the first character,
- * followed by a new line.
+ * Description: This function takes a string pointer as a parameter and prints every other
+ * character of the string, starting with the first character, followed by a new line.
  */
 void puts2(char *str)
 {

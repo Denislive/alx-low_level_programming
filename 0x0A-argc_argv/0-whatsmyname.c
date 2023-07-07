ec00632172ce0e2a7681c546c089550a6b532754
Denislive <dennygachemi6@gmail.com>
2023-07-07 14:30:34 -0700

    prints name followed by newline

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-    /* Unused parameter argc */
-    (void)argc;
+	/* Unused parameter argc */
+	(void)argc;
 
-    /* Print the program name */
-    printf("%s\n", argv[0]);
+	/* Print the program name */
+	printf("%s\n", argv[0]);
 
-    return (0);
+	return (0);
 }

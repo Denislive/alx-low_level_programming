6f884833f544bf426ecc3d612209c4619243f1fc
Denislive <dennygachemi6@gmail.com>
2023-07-18 13:14:53 -0700

    compiled from filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#ifndef __FILENAME_MACRO__
-#define __FILENAME_MACRO__ (__builtin_strrchr(__FILE__, '/') ?	\
+#ifndef FILENAME
+#define FILENAME (__builtin_strrchr(__FILE__, '/') ?	\
 			    __builtin_strrchr(__FILE__, '/') + 1 :	\
 			    __FILE__)
 #endif
 
 int main(void)
 {
-	printf("%s\n", __FILENAME_MACRO__);
+	printf("%s\n", FILENAME);
 	return (0);
 }

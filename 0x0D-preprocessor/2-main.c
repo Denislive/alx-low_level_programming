35772549a78e6f94bf79a72e4cd318f313d29220
Denislive <dennygachemi6@gmail.com>
2023-07-18 12:59:10 -0700

    prints name of file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-#ifndef __FILENAME__
-#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+#ifndef __FILENAME_MACRO__
+#define __FILENAME_MACRO__ (__builtin_strrchr(__FILE__, '/') ?	\
+			    __builtin_strrchr(__FILE__, '/') + 1 :	\
+			    __FILE__)
 #endif
 
 int main(void)
 {
-    printf("%s\n", __FILENAME__);
-    return 0;
+	printf("%s\n", __FILENAME_MACRO__);
+	return (0);
 }

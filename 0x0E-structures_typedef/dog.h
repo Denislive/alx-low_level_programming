ffc2fddf6205ae3c83ed718055692e2649d9e97e
Denislive <dennygachemi6@gmail.com>
2023-07-18 14:22:07 -0700

    prints struct dogt"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Represents a dog with its attributes
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+typedef struct dog
+{
     char *name;
     float age;
     char *owner;
-};
+} dog_t;
 
+/* Function prototypes */
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */

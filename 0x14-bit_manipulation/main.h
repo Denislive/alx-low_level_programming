ba7ed8eab515a61e7539e683a4325ab6de06b31a
Denislive <dennygachemi6@gmail.com>
2023-08-03 15:29:19 -0700

    Func that converts binary to unsigned int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for binary_to_uint */
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+/* Function prototypes for other functions in your project (if any) */
+/* Example:
+ * int some_function(int arg);
+ * void another_function(void);
+ */
+
+#endif /* MAIN_H */

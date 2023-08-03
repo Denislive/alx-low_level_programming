14e87a398b7ea4525c5f447546d13426468c342c
Denislive <dennygachemi6@gmail.com>
2023-08-03 15:44:11 -0700

    added funct that sets value of a bit to 0 at an index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 /* Function prototypes for other functions in your project (if any) */
 /* Example:

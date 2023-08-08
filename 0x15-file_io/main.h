2ffc4f9407294c612915d01e5e1a33c78dcb4a39
Denislive <dennygachemi6@gmail.com>
2023-08-08 05:54:25 -0700

    added fuction that takes text file and prints it to the posix standard output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <unistd.h>
+
+/* Function prototype for read_textfile */
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+/* Function prototypes for other functions in your project (if any) */
+/* Example:
+ * int some_function(int arg);
+ * void another_function(void);
+ */
+
+#endif /* MAIN_H */

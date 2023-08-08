5ee401d5927fba9b668a6298e27b0e6240b9bc06
Denislive <dennygachemi6@gmail.com>
2023-08-08 06:00:02 -0700

    Added a function that creates a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 /* Function prototype for read_textfile */
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
+int create_file(const char *filename, char *text_content);
 
 /* Function prototypes for other functions in your project (if any) */
 /* Example:
